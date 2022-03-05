9bbaba8e1cf0f5aa1d732ad112df3e73b4110422
kierstenv <kierstenvicknair22@gmail.com>
2022-03-05 09:47:21 -0600

    basic question layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+// const quiz = [
+//   {
+//     question: "Q1",
+//     answers: ["a1", "b1", "c1", "d1"]
+//   },
+//   {
+//     question: "Q2",
+//     answers: ["a2", "b2", "c2", "d2"]
+//   },
+//   {
+//     question: "Q3",
+//     answers: ["a3", "b3", "c3", "d3"]
+//   }
+// ]
+
+// const createQuestion = () => {
+//   const questionHead = $("<h1>").text("Q");
+//   const answers = $("<div>").addClass("d-inline-flex flex-column");
+//   const ansA = $("<button>").attr("type", "button").addClass("btn m-1").text("a)");
+//   const ansB = $("<button>").attr("type", "button").addClass("btn m-1").text("b)");
+//   const ansC = $("<button>").attr("type", "button").addClass("btn m-1").text("c)");
+//   const ansD = $("<button>").attr("type", "button").addClass("btn m-1").text("d)");
+  
+//   answers.append(ansA, ansB, ansC, ansD);
+//   $("main").append(questionHead, answers)
+// }
+
 const quiz = [
   {
     question: "Q1",
@@ -16,26 +43,39 @@ const quiz = [
   }
 ]
 
-const createQuestion = () => {
-  const questionHead = $("<h1>").text("Q");
-  const answers = $("<div>").addClass("d-inline-flex flex-column");
-  const ansA = $("<button>").attr("type", "button").addClass("btn m-1").text("a)");
-  const ansB = $("<button>").attr("type", "button").addClass("btn m-1").text("b)");
-  const ansC = $("<button>").attr("type", "button").addClass("btn m-1").text("c)");
-  const ansD = $("<button>").attr("type", "button").addClass("btn m-1").text("d)");
+let questionId = 0;
+
+const createQuestion = function() {
+  const question = quiz[questionId].question;
+  const questionAns = quiz[questionId].incorrectAns;
+  
+  const randomIndex = function(max) {
+    return Math.floor(Math.random() * max);
+  };
+  // randomized question answer choices
+  questionAns.splice(randomIndex(questionAns.length + 1), 0, quiz[questionId].correctAns);
+
+  const questionHead = $("<h1>").text(question);
+  const answers = $("<div>").attr("id", "answer-choice").addClass("d-inline-flex flex-column");
+  const ansA = $("<button>").prop("type", "button").addClass("btn m-1").text("a) " + questionAns[0]);
+  const ansB = $("<button>").prop("type", "button").addClass("btn m-1").text("b) " + questionAns[1]);
+  const ansC = $("<button>").prop("type", "button").addClass("btn m-1").text("c) " + questionAns[2]);
+  const ansD = $("<button>").prop("type", "button").addClass("btn m-1").text("d) " + questionAns[3]);
   
   answers.append(ansA, ansB, ansC, ansD);
-  $("main").append(questionHead, answers)
-}
-  
-$("button, #start-quiz").click(() => {
+  $("main").append(questionHead, answers);
+
+  questionId++;
+};
+
+$("#start-quiz").click(() => {
   $("#start-quiz").remove();
   createQuestion();
 });
 
-$.each(quiz, function() {
-  console.log(this.question)
-  console.log(this.correctAns)
-  console.log(this.incorrectAns)
-  console.log(this.incorrectAns[0])
+$("main").on("click", "#answer-choice", () => {
+  $("main").empty();
+  createQuestion();
 });
+
+// $.inArray();
