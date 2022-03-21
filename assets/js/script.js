7d57ba6a7b51dccbf817586bd52fed6959cbfcc3
kierstenv <kierstenvicknair22@gmail.com>
2022-03-20 20:24:01 -0500

    add & subtract from timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,41 +13,76 @@ const quiz = [
     question: "Q3",
     correctAns: "yay",
     incorrectAns: ["b3", "c3", "d3"]
+  },
+  {
+    question: "Q4",
+    correctAns: "yay",
+    incorrectAns: ["b4", "c4", "d4"]
+  },
+  {
+    question: "Q5",
+    correctAns: "yay",
+    incorrectAns: ["b5", "c5", "d5"]
+  },
+  {
+    question: "Q6",
+    correctAns: "yay",
+    incorrectAns: ["b6", "c6", "d6"]
+  },
+  {
+    question: "Q7",
+    correctAns: "yay",
+    incorrectAns: ["b7", "c7", "d7"]
+  },
+  {
+    question: "Q8",
+    correctAns: "yay",
+    incorrectAns: ["b8", "c8", "d8"]
+  },
+  {
+    question: "Q9",
+    correctAns: "yay",
+    incorrectAns: ["b9", "c9", "d9"]
+  },
+  {
+    question: "Q10",
+    correctAns: "yay",
+    incorrectAns: ["b10", "c10", "d10"]
   }
 ]
 
 let questionId = 0;
 
 const createQuestion = function() {
-  const question = quiz[questionId].question;
-  const questionAns = quiz[questionId].incorrectAns;
+  if(questionId < quiz.length) {
+    const question = quiz[questionId].question;
+    const questionAns = quiz[questionId].incorrectAns;
+    
+    const randomIndex = function(max) {
+      return Math.floor(Math.random() * max);
+    };
+    // randomized question answer choices
+    questionAns.splice(randomIndex(questionAns.length + 1), 0, quiz[questionId].correctAns);
   
-  const randomIndex = function(max) {
-    return Math.floor(Math.random() * max);
-  };
-  // randomized question answer choices
-  questionAns.splice(randomIndex(questionAns.length + 1), 0, quiz[questionId].correctAns);
-
-  const questionHead = $("<h1>").text(question);
-  const answers = $("<div>").attr("id", "answer-choice").addClass("d-inline-flex flex-column");
-  const ansChoices = ["a)", "b)", "c)", "d)"];
+    const questionHead = $("<h1>").text(question);
+    const answers = $("<div>").attr("id", "answer-choice").addClass("d-inline-flex flex-column");
+    const ansChoices = ["a)", "b)", "c)", "d)"];
+    
+    ansChoices.forEach((choice, index) => {
+      const ansButton = $("<button>").prop("type", "button").addClass("btn m-1").text(choice + " " + questionAns[index]);
   
-  ansChoices.forEach(choice => {
-    console.log(ansChoices.indexOf(choice))
-    // $("<button>").prop("type", "button").addClass("btn m-1").text(choice + questionAns[indexOf(choice)]);
-
-    // answers.append(choice);
-  });
-
-  // const ansA = $("<button>").prop("type", "button").addClass("btn m-1").text("a) " + questionAns[0]);
-  // const ansB = $("<button>").prop("type", "button").addClass("btn m-1").text("b) " + questionAns[1]);
-  // const ansC = $("<button>").prop("type", "button").addClass("btn m-1").text("c) " + questionAns[2]);
-  // const ansD = $("<button>").prop("type", "button").addClass("btn m-1").text("d) " + questionAns[3]);
+      if (questionAns[index] === quiz[questionId].correctAns) {
+        ansButton.addClass("correct-ans");
+        // console.log(ansButton.attr("class"));
+      }
   
-  // answers.append(ansA, ansB, ansC, ansD);
-  $("main").append(questionHead, answers);
-
-  questionId++;
+      answers.append(ansButton);
+    });
+  
+    $("main").append(questionHead, answers);
+  
+    questionId++;
+  }
 };
 
 const beginTimer = () => {
@@ -58,18 +93,23 @@ const beginTimer = () => {
   if (timerInt == 1) {
     clearInterval(countdown);
   };
+
+  return timerStr;
 };
 
 const countdown = setInterval(beginTimer, 1000); // BUG countdown starts 2 fast every other reload
 
-// const addTime = (beginTimer)
-
 const timerHandler = (event) => {
   const target = $(event.target);
-  console.log(target);
-  // if ()
-  // timerStr -= 10;
-}
+  const addTime = parseInt($("span.countdown").text()) + 4
+  const subtractTime = parseInt($("span.countdown").text()) - 10
+
+  if (target.hasClass("correct-ans")) {
+    $("span.countdown").text(addTime);
+  } else {
+    $("span.countdown").text(subtractTime);
+  }
+};
 
 $("#start-quiz").click(() => {
   $("#start-quiz").remove();
